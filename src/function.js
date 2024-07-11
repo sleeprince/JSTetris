a07a89b91a146195b0ca085cb2410214a035d852
sleeprince <wowkwangsu@gmail.com>
2024-07-11 12:14:57 +0900

    test

---
+++ src/function.js
@@ -0,0 +1,19 @@
+const model = ["nemo", "niun", "niun_m", ]
+
+const blocks = {
+    nemo : [[1, 1], 
+            [1], [1]],
+    
+
+};
+
+const rotate = () => {};
+const isCrash = () => {};
+const isFull = (row) => {};
+const deleteRows = () => {};
+const shadow = () => {};
+const ground = () => {};
+const draw = () => {};
+
+
+console.log(blocks["nemo"]);
