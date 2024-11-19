95261cd9f2e8ef2249fbdacc1e363f64cf1810fb
노광수-PC\wowkw <wowkwangsu@gmail.com>
2024-11-19 15:50:56 +0900

    [js] 충돌 함수 구현 完, 그림자 블록 그리는 中

--- js/function.js
+++ js/function.js
@@ -6,30 +6,86 @@ export class block {
         this.position = tetrisMap[0].length/2;
         this.rotation = 0;
     }
-
+    moveUp(){
+        this.position -= tetrisMap[0].length;
+    }
+    moveDown(){
+        this.position += tetrisMap[0].length;
+    }
+    moveLeft(){
+        this.position--;
+    }
+    moveRight(){
+        this.position++;
+    }
     rotateR() {
-        if(isCrash()){
-            this.rotation++;
-            this.rotation %= blocks[type].length;
-        }
+        this.rotation++;
+        this.rotation %= blocks[type].length;
     }
     rotateL() {
-        if(isCrash()){
-            this.rotation--;
-            this.rotation += blocks[type].length;
-            this.rotation %= blocks[type].length;
-        }
+        this.rotation--;
+        this.rotation += blocks[type].length;
+        this.rotation %= blocks[type].length;
     }
     isCrash(){
         //블록이 충돌하는지
+        let numOfCols = tetrisMap[0].length;
+        let numOfRows = tetrisMap.length;
+        let cor_x = (this.position % numOfCols) - 2;
+        let cor_y = Math.floor(this.position / numOfCols) - 1;
+        let test_case = blocks[this.type][this.rotation];
+        for(let i = 0; i < test_case.length; i++){
+            for(let j = 0; j < test_case[i].length; j++){
+                if(test_case[i][j] === 1){
+                    let x = cor_x + j;
+                    let y = cor_y + i;
+                    console.log(`x: ${x}, y:${y}`);
+                    if(y < 0){
+                        continue;
+                    }else if(y >= numOfRows || x >= numOfCols || x < 0){
+                        return true;
+                    }else if(tetrisMap[y][x] > -1){                        
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 };
 
 //떨어지는 블록 + 그림자
 export const drawPlayingBlock = (block) => {
-    drawBlock(block);
-    drawShadow(block);
+    let numOfCols = tetrisMap[0].length;
+    let numOfRows = tetrisMap.length;
+    let sizeOfMap = numOfCols * numOfRows;
+    //실제 블록의 위치
+    let loc = block.position;
+    let index = loc - numOfCols - 2;
+    //그림자 블록의 위치
+    let shadow_loc = block.position;
+    block.position = shadow_loc;
+    while(!block.isCrash()){
+        block.moveDown();
+    }
+    block.moveUp();
+    shadow_loc = block.position;
+    block.position = loc;
+    let shadow_index = shadow_loc - numOfCols - 2;
+    console.log(shadow_index);
+    //그리기
+    blocks[block.type][block.rotation].forEach((row, i) => {
+        console.log(row);
+        row.forEach((col, j) => {
+            let id_num = index + j;
+            let shadow_id = shadow_index + j;
+            if(id_num >= 0 && id_num < sizeOfMap && col == 1){
+                //실물
+                document.getElementById(`square_${id_num}`).className += ` ${block.type}`;
+            }
+        });
+        index += numOfCols;
+    });
 };
 
 export const removePlayingBlock = (block) => {
@@ -53,34 +109,11 @@ export const drawGameBoard = () => {
 
 export const deleteRows = () => {};
 
-// 떨어지는 블록 그리기
-const drawBlock = (block) => {
-    let numOfCols = tetrisMap[0].length;
-    let numOfRows = tetrisMap.length;
-    let sizeOfMap = numOfCols * numOfRows;
-    let index = block.position - numOfCols - 2;
-
-    blocks[block.type][block.rotation].forEach((row, i) => {
-        console.log(row);        
-        row.forEach((col, j) => {
-            let id_num = index + j;
-            if(id_num >= 0 && id_num < sizeOfMap && col == 1){
-                document.getElementById(`square_${id_num}`).className += ` ${block.type}`;
-            }
-        });
-        index += numOfCols;
-    });
-
-};
-
-// 떨어지는 블록 그림자 그리기
-const drawShadow = (block) => {};
-
 // next block 그리기
-const drawNextBlock = (block) => {};
+const drawNext = (block) => {};
 
-// saved block 그리기
-const drawSavedBlock = (block) => {};
+// hold block 그리기
+const drawHold = (block) => {};
 
 const detour = () => {}; // 충돌일 때
 const isFull = (row) => {};
